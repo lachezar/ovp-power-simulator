3b47b92c09a9b85b604ea0ca20da26914c93cf30
unknown <yankov@kth.se>
2015-02-05 16:14:09 +0000

    Cycles table now has info about ldr instruction's location

--- generate_cycle_map.py
+++ generate_cycle_map.py
@@ -1,4 +1,6 @@
 import re
+import sys
+import operator
 
 MULS_CYCLES = 1 # could be 32 if no hardware support
 
@@ -21,13 +23,24 @@ def parse_dis_file(file_name):
 def store_table(table, file_name):
   with open(file_name, 'w') as f:
     for entry in table:
-      f.write("%s:%d:%d\n" % entry)
+      f.write("%s:%d:%d:%s:%d\n" % entry)
+      
+def instructions_stats(instructions):
+  stats = dict()
+  for i in instructions:
+    stats[i] = stats.get(i, 0) + 1
+  
+  print 'Instructions stats:'
+  for x in sorted(stats.iteritems(), key=operator.itemgetter(1), reverse=True):
+    print x[0], x[1], "{0:.3f}%".format(100 * (x[1] / float(len(instructions))))
+  return stats
   
 def count_instruction_cycles(i):
   # based on data from: http://web.mit.edu/clarkds/www/Files/slides1.pdf
   branch = False
+  ldr_location = 0
 
-  if i[1] == 'muls':
+  if i[1].startswith('mul'):
     cycles = MULS_CYCLES
   elif i[1] == 'pop':
     cycles = 1 + len(i[2].split(','))
@@ -45,14 +58,21 @@ def count_instruction_cycles(i):
   elif i[1].startswith('b'):
     cycles = 1
     branch = True
-  elif i[1].startswith('ldr') or i[1].startswith('str') or i[1] in ('wfe', 'wfi'):
+  elif i[1].startswith('ldr'):
+    register_location = re.search(r'\[(\w+)(,|\])', i[2]).group(1)
+    if register_location in ('pc', 'sp'):
+      ldr_location = 0xF
+    else:
+      ldr_location = int(register_location.strip('r'))
+    cycles = 2
+  elif i[1].startswith('str') or i[1] in ('wfe', 'wfi'):
     cycles = 2
   elif i[1] == '' or i[1] is None or 'illegal' in i[2]:
     cycles = 0
   else:
     cycles = 1
    
-  return (i[0], cycles, branch)
+  return (i[0], cycles, branch, i[1], ldr_location)
   
 def map_cycles(instructions):
   table = list()
@@ -69,6 +89,11 @@ def map_cycles(instructions):
   return table
       
 if __name__ == '__main__':
-  instructions = parse_dis_file('dis.b.asm')
+  if len(sys.argv) >= 2:
+    filename = sys.argv[1]
+  else:
+    filename = 'dis.b.asm'
+  instructions = parse_dis_file(filename)
   table = map_cycles(instructions)
   store_table(table, 'cycles_map.txt')
+  instructions_stats(map(operator.itemgetter(1), instructions))
