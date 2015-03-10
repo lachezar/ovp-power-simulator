6710e94a79b035ca8f058b90254b9bc7fd290097
unknown <yankov@kth.se>
2015-03-10 11:58:34 +0100

    Added build and run helpers for the spi

---
+++ _build_spi.bat
@@ -0,0 +1,1 @@
+"d:\MentorGraphics\Sourcery_CodeBench_Lite_for_ARM_EABI\bin\arm-none-eabi-objdump.exe" -D -l --target ihex -Mforce-thumb -marm "../../../Documents/master thesis/hi/hi2/Src/nrf51822/Board/nrf6310/spi_master_example/gcc/_build/spi_master_example_gcc_xxaa.hex" > ./application.asm 
