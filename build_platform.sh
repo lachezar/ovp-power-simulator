305d3bf1e31446e2e0c91f4f036734b88212d76c
unknown <yankov@kth.se>
2015-04-20 15:27:20 +0100

    Added some example files to build and run the simulator

---
+++ build_platform.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+make -f Makefile NOVLNV=1 OPT=1
+make -C src/pse_timer NOVLNV=1 OPT=1
+make -C src/pse_rtc NOVLNV=1 OPT=1
+make -C src/pse_radio NOVLNV=1 OPT=1
+make -C src/pse_spi NOVLNV=1 OPT=1
+
---
+++ run_example_blinky.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+./platform.Linux32.exe ~/nordic/sdk/nrf51822/Board/pca10001/blinky_example/gcc/_build/blinky_gcc_xxaa.hex ~/nordic/sdk/nrf51822/Board/pca10001/blinky_example/gcc/_build/blinky_gcc_xxaa.asm 
+
